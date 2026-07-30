d37eccbd7df46e2d7ce00306f6bfcb3396f8c676
Nashid <nashid@streamly.app>
2026-07-30 17:06:23 +0530

    Code cleanup: remove scratch files and optimize production builds

--- check_braces.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/components/Navbar.tsx', 'utf8');
-let openCount = 0;
-for(let i=0; i<content.length; i++) {
-  if (content[i] === '{') openCount++;
-  if (content[i] === '}') openCount--;
-}
-console.log('Brace count:', openCount);
--- fix_escaping.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/Navbar.tsx', 'utf8');
-code = code.replace(/\\`/g, '`');
-fs.writeFileSync('src/components/Navbar.tsx', code);
